e07b7dbf2bfcc867f15cd2e5675b08e80589b98a
marcoswlrich <marcosapw.dev@gmail.com>
2023-06-29 01:11:52 -0400

    Classes CS50

---
+++ cs50/c/celsius.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int lower, upper, step;
+  float fahr, celsius;
+  lower = 0;
+  upper = 300;
+  step = 20;
+  fahr = lower;
+
+	  while (fahr <= upper) {
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%4.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
+
+
+
